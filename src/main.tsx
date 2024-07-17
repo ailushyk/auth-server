25b46ff9bcd0765661d41976569bf47ad6f563bf
Alex Ilushyk <ailushyk@gmail.com>
2024-07-17 20:22:17 +0200

    feat: add custom theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+/* eslint-disable react-refresh/only-export-components */
+import { lazy, StrictMode, Suspense } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+// The following block can be uncommented to test a specific page with `yarn dev`
+// Don't forget to comment back or your bundle size will increase
+/*
+import { getKcContextMock } from "./keycloak-theme/login/KcPageStory";
+
+if (import.meta.env.DEV) {
+    window.kcContext = getKcContextMock({
+        pageId: "register.ftl",
+        overrides: {}
+    });
+}
+*/
+
+const KcLoginThemePage = lazy(() => import('./keycloak-theme/login/KcPage'))
+const KcAccountThemePage = lazy(() => import('./keycloak-theme/account/KcPage'))
+const App = lazy(() => import('./App'))
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Suspense>
+      {(() => {
+        switch (window.kcContext?.themeType) {
+          case 'login':
+            return <KcLoginThemePage kcContext={window.kcContext} />
+          case 'account':
+            return <KcAccountThemePage kcContext={window.kcContext} />
+        }
+        return <App />
+      })()}
+    </Suspense>
+  </StrictMode>,
 )
+
+declare global {
+  interface Window {
+    kcContext?:
+      | import('./keycloak-theme/login/KcContext').KcContext
+      | import('./keycloak-theme/account/KcContext').KcContext
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { keycloakify } from 'keycloakify/vite-plugin'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    keycloakify({
+      keycloakVersionTargets: {
+        hasAccountTheme: true,
+        '21-and-below': false,
+        '23': false,
+        '24': false,
+        '25-and-above': 'keycloak-theme.jar',
+      },
+    }),
+  ],
 })
